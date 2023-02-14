866daf454a567e8323859da5e50121cfd2ff8f8e
DESKTOP-EIEQFRR\Ollie <owoo0002@student.monash.edu>
2023-02-14 09:34:45 +0800

    standardised language key names

--- main.py
+++ main.py
@@ -6,6 +6,11 @@ SOURCE_FILE_PATH = 'source.html'
 OUTPUT_FILE_PATH = '500_chengyu' # DO NOT INCLUDE .json FILE EXTENSION
 OUTPUT_MINIFIED_FILE_PATH = OUTPUT_FILE_PATH + '_min' # DO NOT INCLUDE .json FILE EXTENSION
 
+SIMPLIFIED_CHINESE_KEY_NAME = "zhCN"
+TRADITIONAL_CHINESE_KEY_NAME = "zhHK"
+ENGLISH_KEY_NAME = "enGB"
+PINYIN_KEY_NAME = "zhPY"
+
 def isNodeAChengyuHeading(node):
     if node.name == 'h2':
         return True
@@ -25,14 +30,16 @@ def exampleChineseSentenceNodetoString(node):
             out += item.contents[0]
     return out.strip()
 
-def saveVarToJsonFile(var, filename, pretty = False):
-    if pretty:
-        indent = 4
-    else:
+def saveVarToJsonFile(var, filename, minify = False):
+    if minify:
         indent = None
+        seperators = (',', ':')
+    else:
+        indent = 2
+        seperators = (', ', ': ')
     
     with open(filename, 'w', encoding="utf-8") as f:
-        json.dump(var, f, indent=indent, ensure_ascii=False)
+        json.dump(var, f, indent=indent, separators=seperators, ensure_ascii=False)
         
 
 with open(SOURCE_FILE_PATH, 'r', encoding="utf-8") as f:
@@ -85,31 +92,32 @@ for entry in entry_list:
     current_entry = {}
     
     current_entry['phrase'] = {
-        'zh-CN': re.search("\[(.+)\]", entry[0].a.contents[0]).group(1),
-        'zh-HK': re.search("\((.+)\)", entry[0].a.contents[0]).group(1),
-        'pinyin': re.search("\)\s*(.+)\Z", entry[0].a.contents[0]).group(1),
+        SIMPLIFIED_CHINESE_KEY_NAME: re.search("\[(.+)\]", entry[0].a.contents[0]).group(1),
+        TRADITIONAL_CHINESE_KEY_NAME: re.search("\((.+)\)", entry[0].a.contents[0]).group(1),
+        PINYIN_KEY_NAME: re.search("\)\s*(.+)\Z", entry[0].a.contents[0]).group(1),
     }
     
-    current_entry['translations'] = [
-        {
-            'english': entry[1].contents[0].string,
-        },
-    ]
+    current_entry['translations'] = {
+        ENGLISH_KEY_NAME: entry[1].contents[0].string,
+    }
     
     current_entry['examples'] = [
         {
-            'simplified': exampleChineseSentenceNodetoString(entry[2]),
-            'pinyin': entry[3].contents[0].string,
-            'english': entry[4].contents[0].string,
+            SIMPLIFIED_CHINESE_KEY_NAME: exampleChineseSentenceNodetoString(entry[2]),
+            PINYIN_KEY_NAME: entry[3].contents[0].string,
+            ENGLISH_KEY_NAME: entry[4].contents[0].string,
         },
         {
-            'simplified': exampleChineseSentenceNodetoString(entry[5]),
-            'pinyin': entry[6].contents[0].string,
-            'english': entry[7].contents[0].string,
+            SIMPLIFIED_CHINESE_KEY_NAME: exampleChineseSentenceNodetoString(entry[5]),
+            PINYIN_KEY_NAME: entry[6].contents[0].string,
+            ENGLISH_KEY_NAME: entry[7].contents[0].string,
         },
     ]
     
+    for i in range(8, len(entry)):
+        pass
+    
     chengyu_list['entries'].append(current_entry)
 
-saveVarToJsonFile(chengyu_list, OUTPUT_FILE_PATH + '.json', True)
-saveVarToJsonFile(chengyu_list, OUTPUT_MINIFIED_FILE_PATH + '.json')
+saveVarToJsonFile(chengyu_list, OUTPUT_FILE_PATH + '.json', False)
+saveVarToJsonFile(chengyu_list, OUTPUT_MINIFIED_FILE_PATH + '.json', True)
